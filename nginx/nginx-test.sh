c643b9e675b803c8001194cfe2957b86fdc6e798
Ryo Nakamura <upa@haeena.net>
2017-09-02 15:36:37 +0900

    add graft nginx siege result

--- nginx/nginx-test.sh
+++ nginx/nginx-test.sh
@@ -5,28 +5,38 @@ target_nat=10.0.0.2:8080
 outputdir=output
 trynum=20
 
-conn=1
+conn=50
 reqnum=100000
-
-for s in 64 256 1k 4k 16k 64k 256k 1m 4m 16m 64m; do
-for x in `seq -w 1 $trynum`; do
-	
-	echo $s byte, target graft, count $x
-	ab -n $reqnum -c $conn http://$target_graft/${s}.img \
-		> $outputdir/graft_ab_${s}_${x}.txt
-	# wait for TIME_WAIT disappear
-	sleep 120
-done
-done
+time=30s
 
 <<OUT
 for s in 64 256 1k 4k 16k 64k 256k 1m 4m 16m 64m; do
 for x in `seq -w 1 $trynum`; do
-
-	echo $s byte, target nat, count $x
-	siege -b -c $conc -t $conc http://$target_nat/${s}.img \
-		--log=$outputdir/nat_siege_${s}_${x}.txt
+	
+	echo $s byte, target graft, count $x
+	echo \
+	siege -b -c $conn -t $time http://$target_graft/${s}byte.img \
+		--log=$outputdir/graft_siege_conc-${conn}_${s}.txt
+	siege -b -c $conn -t $time http://$target_graft/${s}byte.img \
+		--log=$outputdir/graft_siege_conc-${conn}_${s}.txt
+	# wait for TIME_WAIT disappear
 	sleep 70
 done
 done
 OUT
+
+for conn in 1 50; do
+for s in 64 256 1k 4k 16k 64k 256k 1m 4m 16m 64m; do
+for x in `seq -w 1 $trynum`; do
+	
+	echo $s byte, target nat, count $x
+	echo \
+	siege -b -c $conn -t $time http://$target_nat/${s}byte.img \
+		--log=$outputdir/nat_siege_conc-${conn}_${s}.txt
+	siege -b -c $conn -t $time http://$target_nat/${s}byte.img \
+		--log=$outputdir/nat_siege_conc-${conn}_${s}.txt
+	# wait for TIME_WAIT disappear
+	sleep 70
+done
+done
+done
